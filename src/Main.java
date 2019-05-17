3410c9e01f287fd2a1282f70e7197c67148224ee
sadeghsalimi <ssalimi@ce.sharif.edu>
2019-05-17 14:18:37 +0430

    Writers created.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import Threads.Writer;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            FileWriter writer = new FileWriter("data.txt");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 1; i < 4; i++) {
+            new Writer(i).start();
+        }
     }
 }
--- src/Threads/Writer.java
+++ src/Threads/Writer.java
@@ -1,8 +1,49 @@
 package Threads;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 public class Writer extends Thread {
+    private int number;
+    private Random random = new Random();
+
+    public Writer(int number) {
+        this.number = number;
+    }
+
     @Override
     public void run() {
+        while (true) {
+            writeInFile();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    private void writeInFile() {
+        String toWrite = getNextToWrite();
+        // TODO: 5/17/19
+        System.out.println("I wrote: \"" + toWrite + "\"");
+    }
+
+    private String getNextToWrite() {
+        switch (number) {
+            case 1:
+                int numberOfAlphabets = 'Z' - 'A' + 1;
+                char nextChar = (char) (random.nextInt(numberOfAlphabets) + 'A');
+                return nextChar + "";
+            case 2:
+                int randomNumber = random.nextInt(100) + 1;
+                return randomNumber + "";
+            case 3:
+                char[] chars = {'!', '@', '#', '$', '%', '&', '*'};
+                char randomChar = chars[random.nextInt(chars.length)];
+                return randomChar + "";
+        }
+        return null;
     }
 }
